b8c5937e98bfc03aa07f2023c3fa9f9f1a3f1174
Nagireddy P <nagireddy.panditi@gmail.com>
2020-02-28 20:39:13 +0530

    changes added in components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import  Header  from './Components/Header';
+import { Balance } from './Components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './Components/AddTransaction';
+import { GlobalProvider } from './Context/GlobalState';
 import './App.css';
-import Header from './Components/Header'
 
 function App() {
   return (
-    <div className="App">
+    <GlobalProvider>
       <Header />
-    </div>
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
